4c1daf6da9c2e7f21246568d3fdabb00b9f0ac8d
erichlf <erichlf@gmail.com>
2013-11-09 12:40:10 +0100

    Added an extremely simple conky weather.

---
+++ weather.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#wunderground RSS weather tool for conky
+#
+#USAGE: weather.sh <locationcode>
+#
+#addapted from Michael Seiler's weather script for Accuweather 2007
+# Erich L Foster 2013
+
+METRIC=1 #Should be 0 or 1; 0 for F, 1 for C
+
+if [ ${METRIC} ]; then
+    METRIC='metric'
+else
+    METRIC='english'
+fi
+
+if [ -z $1 ]; then
+    echo
+    echo "USAGE: weather.sh <locationcode>"
+    echo
+    exit 0;
+fi
+
+curl -s http://rss.wunderground.com/auto/rss_full/global/stations/$1.xml\?units=${METRIC} | perl -ne 'if (/Current/) {chomp;/\<title\>Current Conditions : (\d+[CF]), (.+) - \d.+\<\/title\>/; print "$1 $2"; }'
