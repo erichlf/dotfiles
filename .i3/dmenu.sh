0cba137c1f3f4c2489ea44582d5cd5119d45523f
erichlf <erichlf@gmail.com>
2013-11-19 13:16:54 +0100

    Fixed dmenu+yeganesh so that it executes programs.

--- .i3/dmenu.sh
+++ .i3/dmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dmenu_path | ~/.cabal/bin/yeganesh -- -fn "xft:Monospace 9" -nb "#101010" -nf "#b492b6" -sb "#191919" -sf "#b72f62" -b
+$(dmenu_path | ~/.cabal/bin/yeganesh -- -fn "xft:Monospace 9" -nb "#101010" -nf "#b492b6" -sb "#191919" -sf "#b72f62" -b)
