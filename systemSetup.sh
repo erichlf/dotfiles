5b1cf0fc36878b728635c2e44f15d685e228cb5f
Erich L Foster <erichlf@gmail.com>
2015-09-07 08:56:48 +0200

    Added scrot to base system.

--- systemSetup.sh
+++ systemSetup.sh
@@ -59,7 +59,7 @@ sudo gem install mdprev
 sudo apt-get install -y i3 conky curl arandr gtk-redshift ttf-ancient-fonts \
                         acpi gtk-doc-tools gobject-introspection \
                         libglib2.0-dev cabal-install htop feh python-feedparser \
-                        python-keyring xbacklight bikeshed autocutsel
+                        python-keyring xbacklight bikeshed autocutsel scrot
 
 #bikeshed contains utilities such as purge-old-kernels
 
