f6f6b262367a620ce13aea8e506df35e0a0e48cc
Erich L Foster <erichlf@gmail.com>
2020-11-11 08:46:35 -0500

    Add 1Pass

--- systemSetup.sh
+++ systemSetup.sh
@@ -180,6 +180,12 @@ function LaTeX(){
 #bikeshed contains utilities such as purge-old-kernels
 function base_sys(){
   cd $HOME
+
+  # setup repo for 1password
+  sudo apt-key --keyring /usr/share/keyrings/1password.gpg adv --keyserver keyserver.ubuntu.com \
+       --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+  echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password.gpg] https://downloads.1password.com/linux/debian edge main' | sudo tee /etc/apt/sources.list.d/1password.list
+
   if no_ppa_exists alessandro-strada
   then
       add_ppa alessandro-strada/ppa
@@ -191,11 +197,7 @@ function base_sys(){
   fi
 
   apt_install wget curl bashtop iftop cifs-utils nfs-common autofs google-drive-ocamlfuse gnome-tweak-tool \
-              nautilus-dropbox pass
-
-  dropbox start -i
-
-  ln -s ~/DropBox/org ~/org
+              1password pass
 
   if [ ! -d /media/NFS ]; then
     sudo mkdir /media/NFS
