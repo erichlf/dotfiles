0d3422ff3f606dfac62360f7334c188f468f4bbf
Erich L Foster <erichlf@gmail.com>
2020-12-09 13:25:45 -0500

    Fix Some Issues with emacsclient

--- systemSetup.sh
+++ systemSetup.sh
@@ -148,7 +148,16 @@ function dev_tools(){
   mkdir -p $HOME/.config/systemd/user
   ln -s $DOTFILES_DIR/emacs.service $HOME/.config/systemd/user/emacs.service
   systemctl --user enable --now emacs
-
+  # install source code pro fonts
+  mkdir -p /tmp/adodefont
+  cd /tmp/adodefont
+  wget -q --show-progress -O source-code-pro.zip https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
+  unzip -q source-code-pro.zip -d source-code-pro
+  fontpath=/usr/local/share/fonts/
+  sudo cp -v source-code-pro/*/OTF/*.otf $fontpath
+  fc-cache -f
+  rm -rf source-code-pro{,.zip}
+  cd $DOTFILES_DIR
 
   # setup link for powerline
   cd $HOME/.config/
