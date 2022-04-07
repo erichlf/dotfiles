56dede76369aca909521b55b80538ab1dc869fa0
Erich L Foster <erichlf@gmail.com>
2022-04-07 09:08:10 -0400

    Update Robot Subnets and Various Annoyances

--- my-emacs.el
+++ my-emacs.el
@@ -19,6 +19,14 @@
 
 (require 'org-tempo)
 
+;; pretty colors during compilation
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
 ;; org-page
 (require 'org-page)
 (setq op/repository-directory "~/workspace/erichlf.github.io")
