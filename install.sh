b31aac20b80959f920baf81eacbbbc904765320e
Erich L Foster <erichlf@gmail.com>
2023-11-28 09:19:41 +0100

    Add dotfiles install for devcontainers

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ln -sf .gitconfig $HOME/.gitconfig
+ln -sf .gitexcludes $HOME/.gitexcludes
+
+# powerline fonts for zsh agnoster theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+ln -sf .zshrc $HOME/.zshrc
