f77fd5edb5a2b838fcfb4e228f3bf6ca2a9e6754
Erich L Foster <erichlf@gmail.com>
2023-11-28 09:33:02 +0100

    Don't use links

--- devcontainer_install.sh
+++ devcontainer_install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-ln -sf .gitconfig $HOME/.gitconfig
-ln -sf .gitexcludes $HOME/.gitexcludes
+DOTFILES=$(pwd)
+
+cd $HOME
+
+cat $DOTFILES/.bashrc >> ~/.bashrc
+
+ln -sf $DOTFILES/.gitconfig
+ln -sf $DOTFILES/.gitexcludes
 
 # powerline fonts for zsh agnoster theme
 git clone https://github.com/powerline/fonts.git
@@ -10,7 +16,8 @@ cd fonts
 cd .. && rm -rf fonts
 
 # oh-my-zsh & plugins
+ln -sf $DOTFILES/.oh-my-zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
-ln -sf .zshrc $HOME/.zshrc
+ln -sf $DOTFILES/.zshrc
