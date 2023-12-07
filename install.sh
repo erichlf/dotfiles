9859e1dbedd53331d14d2327a251e81c5070b6e8
Erich L Foster <erichlf@gmail.com>
2023-12-07 17:45:58 +0100

    Update for phone

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ 
+pkg install getconf termux-tools vim rsync openssh termux-api which -y
 
 DOTFILES=$HOME/dotfiles
 
@@ -8,6 +10,10 @@ git submodule init
 git submodule update
 
 cd $HOME
+mkdir -p $HOME/.termux
+
+# termux settings
+ln -sf $DOTFILES/termux.properties $HOME/.termux/termux.properties
 
 # spacevim setup
 ln -sf $DOTFILES/SpaceVim $HOME/.vim
@@ -18,19 +24,21 @@ ln -sf $DOTFILES/.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES/.gitexcludes $HOME/.gitexcludes
 
 # oh-my-bash & plugins
-cat $DOTFILES/.bashrc >> $HOME/.bashrc
+ln -sf $DOTFILES/.bashrc $HOME/.bashrc
 ln -sf $DOTFILES/.aliases $HOME/.aliases
 ln -sf $DOTFILES/.exports $HOME/.exports
 ln -sf $DOTFILES/.oh-my-bash $HOME/.oh-my-bash
 
 # setup starship
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
-unzip DroidSansMono.zip -d ~/.fonts
-fc-cache -fv
+curl -sSL https://github.com/prateekpunetha/termux-setup/raw/main/fonts/font.ttf -o $HOME/.termux/font.ttf
+termux-reload-settings
 
 curl -sS https://starship.rs/install.sh -o starship.sh 
 chmod +x starship.sh
-sudo ./starship.sh -y
+./starship.sh -y --bin-dir /data/data/com.termux/files/usr/bin
+mkdir -p $HOME/.config
 starship preset pastel-powerline > $HOME/.config/starship.toml
 rm -f starship.sh
- 
+
+yes | pkg remove nano
+
