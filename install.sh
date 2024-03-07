10622d51c228915ef9c4339a3fdaf41a6076a8c0
Erich L Foster <erichlf@gmail.com>
2024-03-07 16:27:39 +0000

    Update for retropi

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
  
-pkg install getconf termux-tools vim \
-  rsync openssh termux-api which make \
-  zsh fzf python-pip stow -y
+sudo apt install vim rsync zsh fzf stow wget unzip gawk -y
 
-pip install pygments
-
-chsh -s zsh
+sudo usermod --shell /usr/bin/zsh $USER
 
 DOTFILES_DIR=$HOME/dotfiles
 
@@ -16,25 +12,23 @@ git submodule init
 git submodule update
 
 mkdir -p $HOME/.config
-mkdir -p $HOME/.termux
 
 stow -v --adopt --dir $DOTFILES_DIR --target $HOME --restow my-home
 stow -v --adopt --dir $DOTFILES_DIR/private/ --target $HOME/.ssh --restow .ssh
 stow -v --adopt --dir $DOTFILES_DIR --target $HOME/.config/ --restow starship
 # this relies on my-home being stowed already
 stow -v --adopt --dir $DOTFILES_DIR --target $HOME/.oh-my-zsh/custom/plugins/ --restow zsh
-stow -v --adopt --dir $DOTFILES_DIR --target $HOME/.termux/ --restow termux
 # if the adopt made a local change then undo that
 git checkout HEAD -- starship zsh my-home private
  
 # setup starship
-curl -sSL https://github.com/prateekpunetha/termux-setup/raw/main/fonts/font.ttf -o $HOME/.termux/font.ttf
-termux-reload-settings
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+unzip DroidSansMono.zip -d $HOME/.fonts
+fc-cache -fv
+rm -f DroidSansMono.zip
 
 curl -sS https://starship.rs/install.sh -o starship.sh 
 chmod +x starship.sh
-./starship.sh -y --bin-dir /data/data/com.termux/files/usr/bin
+./starship.sh -y 
 rm -f starship.sh
-
-yes | pkg remove nano
 
