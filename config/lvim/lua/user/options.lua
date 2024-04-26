1f900de571498a6b03a0e2af0788312da28b03b8
Erich L Foster <erichlf@gmail.com>
2024-04-26 13:08:01 +0200

    Fix lsp install

--- config/lvim/lua/user/options.lua
+++ config/lvim/lua/user/options.lua
@@ -72,19 +72,16 @@ table.insert(lvim.builtin.alpha.dashboard.section.buttons.entries,
 )
 
 -- LSP
-lvim.lsp.installer.setup.automatic_installation = true
-require("mason-lspconfig").setup {
-  ensure_installed = {
-    "clangd",
-    "cmake",
-    "docker_compose_language_service",
-    "dockerls",
-    "lemminx",
-    "lua_ls",
-    "pyright",
-    "taplo",
-    "yamlls",
-  },
+lvim.lsp.installer.setup.ensure_installed = {
+  "clangd",
+  "cmake",
+  "docker_compose_language_service",
+  "dockerls",
+  "lemminx",
+  "lua_ls",
+  "pyright",
+  "taplo",
+  "yamlls",
 }
 require("lvim.lsp.null-ls.formatters").setup {
   { name = "black" },
