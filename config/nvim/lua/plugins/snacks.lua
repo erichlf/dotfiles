b260bc7e22d7b1f7ef0658eb12b3c56d978ea0d0
Erich L Foster <erichlf@gmail.com>
2025-03-05 20:47:42 +0100

    Add devcontainer bringup to snacks.dashboard

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      local keys = opts.dashboard.preset.keys
+      opts.dashboard.preset.keys[#keys + 1] = {
+        action = "<CMD>lua require('devcontainer-cli.devcontainer_cli').up()<CR>",
+        desc = " Bringup Devcontainer",
+        icon = LazyVim.config.icons.kinds.Package,
+        key = "D",
+      }
+
+      return opts
+    end,
+  },
+}
