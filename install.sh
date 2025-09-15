17c2ddd2f7c35fa1b06a6e7cab07c4002c8d12db
Erich L Foster <erichlf@gmail.com>
2025-09-15 12:33:12 -0700

    Simplify installer

--- install.sh
+++ install.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
 set -e
+shopt -s expand_aliases
 
-# get the kernel string
-kernel=$(uname -r)
-case "$kernel" in
-  *"android"* )
-    ./scripts/phone.sh
-    ;;
-  *"azure"* ) # github action
-    export CI=true
-    if [[ $DEV_WORKSPACE != "" ]]; then
-      ./scripts/devcontainer.sh
-    else
-      ./scripts/main.sh
-    fi
-    ;;
-  *"MANJARO"* )
-    if [[ $DEV_WORKSPACE != "" ]]; then
-      ./scripts/devcontainer.sh
-    else
-      ./scripts/main.sh
-    fi
-    ;;
-  *"qnap"* )
-    ./scripts/nas.sh
-    ;;
-  *)
-    if [[ $(uname -o) == "Darwin" ]]; then
-      ./scripts/mac.sh
-    else
-      echo \
-"
-  Unknown system: $(uname -a) 
-  KTHNXBYE
-"
-      exit 1
-    fi
-    ;;
-esac
+SYSTEM="DEVCONTAINER"
+cd "$(dirname "$0")/.."
+DOTFILES_DIR=$(pwd)
 
+source "$DOTFILES_DIR/scripts/utils.sh"
+
+print_details
+
+apt_update
+apt_install \
+  fzf \
+  golang-go \
+  npm \
+  pass \
+  python3-venv \
+  software-properties-common \
+  stow \
+  wget \
+  unzip \
+  zsh
+
+# change to zsh as default shell
+sudo chsh -s /usr/bin/zsh
+
+# ensure that .config is owned by the current user
+if [[ -d $HOME/.config && ! $(stat -c "%U" "$HOME/.config") == "$(whoami)" ]]; then
+  sudo chown $(id -u):$(id -g) "$HOME/.config"
+fi
+
+INFO "Installing NEOVIM..."
+apt_install libfuse2 fuse3
+wget https://github.com/neovim/neovim-releases/releases/download/v0.11.4/nvim-linux-x86_64.appimage
+sudo mv nvim.appimage /usr/bin/nvim
+sudo chmod u+x /usr/bin/nvim
+pip3 install debugpy
